2166bb53f2d4e7d207fc48a56508e80e2c64610d
Tebogo Mosiane <mosianets@gmail.com>
2024-05-30 13:13:29 +0200

    Refactored state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,33 +1,48 @@
 import React from "react"
+import memesData from "../memesData.js"
 
 export default function Meme() {
+   
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
     return (
         <main>
-            <form className="form">
-                <div>
-                    <label htmlFor="top-text">Top Text</label>
-                    <input
-                        id="top-text"
-                        type="text"
-                        placeholder="Shut up"
-                        className="form--input"
-                    />
-                </div>
-                <div>
-                    <label>Bottom Text
-                    <input
-                            type="text"
-                            placeholder="and take my money"
-                            className="form--input"
-                        />
-                    </label>
-                </div>
-                <button
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </form>
+            </div>
+            <img src={meme.randomImage} className="meme--image" />
         </main>
     )
 }
